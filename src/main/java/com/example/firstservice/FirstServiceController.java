d44de4811dff741322707535ce76907ba37afc55
Andrew Park <izttotio@hanmail.net>
2023-11-12 20:29:14 +0900

    test

--- src/main/java/com/example/firstservice/FirstServiceController.java
+++ src/main/java/com/example/firstservice/FirstServiceController.java
@@ -18,7 +18,7 @@ public class FirstServiceController {
     @GetMapping("/message")
     public String message(@RequestHeader("first-request") String header) {
         log.info(header);
-        //aaatt
+        //aaattest
         return "Hello World in First Service";
     }
 }
