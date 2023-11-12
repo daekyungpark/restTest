6956bad729ac368e4a838b9bdc35c3f46711c941
Andrew Park <izttotio@hanmail.net>
2023-11-12 18:47:45 +0900

    jenkinsa

--- src/main/java/com/example/firstservice/FirstServiceController.java
+++ src/main/java/com/example/firstservice/FirstServiceController.java
@@ -18,7 +18,7 @@ public class FirstServiceController {
     @GetMapping("/message")
     public String message(@RequestHeader("first-request") String header) {
         log.info(header);
-        //testaaabc
+        //testaaabca
         return "Hello World in First Service";
     }
 }
